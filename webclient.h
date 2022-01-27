e976312bc6c9036e5af53b8b1fa60eed0d025703
milo2708 <92578029+milo2708@users.noreply.github.com>
2022-01-27 18:22:17 +0100

    Update webclient.h

--- webclient.h
+++ webclient.h
@@ -1,7 +1,8 @@
 #ifndef WEBCLIENT_H
 #define WEBCLIENT_H
-
 #include "ui_webclient.h"
+#include <QAbstractSocket>
+#include <QTcpSocket>
 
 class Webclient : public QWidget, private Ui::Webclient
 {
@@ -9,6 +10,14 @@ class Webclient : public QWidget, private Ui::Webclient
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+private slots:
+    void on_button_clicked();
+    void tcp_connect();
+    void tcp_receive();
+private:
+    QTcpSocket* m_socket = nullptr;
+    const unsigned short m_port = 80;
+    QByteArray m_hostname;
 };
 
 #endif // WEBCLIENT_H
